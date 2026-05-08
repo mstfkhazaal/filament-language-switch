8a67c7c8a1021526afa68a4aafdd7a0544917001
mstfkhazaal <47450090+mstfkhazaal@users.noreply.github.com>
2026-05-08 11:26:44 +0300

    Refactor language switch panel display logic

--- resources/views/language-switch.blade.php
+++ resources/views/language-switch.blade.php
@@ -5,19 +5,18 @@
     $isFlagsOnly = $languageSwitch->isFlagsOnly();
     $isFixedOutsidePanels = $languageSwitch->isFixedOutsidePanels();
 
-    // RTL Support Logic
     $isRtl = in_array(app()->getLocale(), ['ar', 'fa', 'he', 'ur'], true);
     $defaultPlacement = $isRtl ? 'bottom-start' : 'bottom-end';
 
     $placement = match (true) {
         $outsidePanelsPlacement === 'top-center' && $isFlagsOnly => 'bottom',
         $outsidePanelsPlacement === 'bottom-center' && $isFlagsOnly => 'top',
-        !$isVisibleOutsidePanels && $isFlagsOnly => 'bottom',
+        ! $isVisibleOutsidePanels && $isFlagsOnly => 'bottom',
         default => $defaultPlacement,
     };
 
     $displayAs = $languageSwitch->getDisplayAs();
-    $isUserMenuItem = $languageSwitch->isRenderedAsUserMenuItem() && !$isVisibleOutsidePanels;
+    $isUserMenuItem = $languageSwitch->isRenderedAsUserMenuItem() && ! $isVisibleOutsidePanels;
 
     $beforeSpace = $languageSwitch->getOutsidePanelBeforeSpace();
     $afterSpace = $languageSwitch->getOutsidePanelAfterSpace();
@@ -25,21 +24,23 @@
 
 <div>
     @if ($isVisibleOutsidePanels)
-        <div @class([
-            'fls-display-on w-full flex p-4 z-10 pointer-events-none',
-            'fixed' => $isFixedOutsidePanels,
-            'absolute' => ! $isFixedOutsidePanels,
-            'top-0' => str_contains($outsidePanelsPlacement, 'top'),
-            'bottom-0' => str_contains($outsidePanelsPlacement, 'bottom'),
-            'justify-start' => str_contains($outsidePanelsPlacement, 'left'),
-            'justify-end' => str_contains($outsidePanelsPlacement, 'right'),
-            'justify-center' => str_contains($outsidePanelsPlacement, 'center'),
-        ])>
+        <div
+            @class([
+                'fls-display-on w-full flex p-4 z-[9998] pointer-events-none',
+                'fixed' => $isFixedOutsidePanels,
+                'absolute' => ! $isFixedOutsidePanels,
+                'top-0' => str_contains($outsidePanelsPlacement, 'top'),
+                'bottom-0' => str_contains($outsidePanelsPlacement, 'bottom'),
+                'justify-start' => str_contains($outsidePanelsPlacement, 'left'),
+                'justify-end' => str_contains($outsidePanelsPlacement, 'right'),
+                'justify-center' => str_contains($outsidePanelsPlacement, 'center'),
+            ])
+        >
             <div
                 class="pointer-events-auto"
                 style="
-                    @if($beforeSpace) margin-inline-start: {{ $beforeSpace }}; @endif
-                    @if($afterSpace) margin-inline-end: {{ $afterSpace }}; @endif
+                    @if ($beforeSpace) margin-inline-start: {{ $beforeSpace }}; @endif
+                    @if ($afterSpace) margin-inline-end: {{ $afterSpace }}; @endif
                 "
             >
                 @include('language-switch::switch')
